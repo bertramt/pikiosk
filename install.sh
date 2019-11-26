54da4fe3c2a1c4de383430a1ebf72e75a40136fd
Tim Bertram <bertramt@gmail.com>
2019-11-26 15:05:50 -0600

    Install updates

--- install.sh
+++ install.sh
@@ -15,11 +15,15 @@ sudo apt install -y xinit xserver-xorg chromium-browser htop numlockx tmux
 while true; do
     read -p "Do you wish to install extra RPi-Mon package?" yn
     case $yn in
-        [Yy]* ) sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list;  sudo apt install -y dirmngr;sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F;sudo apt update && sudo apt install rpimonitor;sudo /etc/init.d/rpimonitor update;break;;
+        [Yy]* ) sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list;  sudo apt install -y dirmngr;sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F;sudo apt update && sudo apt install -y rpimonitor;sudo /etc/init.d/rpimonitor update;break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+echo ""
+echo "Apt installs complete"
+echo ""
 
 # Find all dot files then if the original file exists, create a backup
 # Once backed up to {file}.dtbak symlink the new dotfile in place
@@ -30,4 +34,5 @@ for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
     ln -s $PWD/$file ~/$file
 done
 
+echo ""
 echo "Installed Please Reboot"
