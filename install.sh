e4e5d4d8c57f10444229cb07eb27fa5e52f4ae54
Tim Bertram <bertramt@gmail.com>
2019-12-03 16:51:29 -0600

    Test for auto adding to cmdline.txt

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@
     #sudo apt update && sudo apt -y install vim-scripts
 #fi
 
+# Install Updates and other required packages
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y xinit xserver-xorg chromium-browser htop numlockx tmux fbi
 
+#Check if rpimonitor is installed and offer to install it.
 if [ $(dpkg-query -W -f='${Status}' rpimonitor 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
     while true; do
@@ -40,6 +42,7 @@ for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
     ln -s $PWD/$file ~/$file
 done
 
+#Link the splash image to /opt dir
 SPLASHIMAGE=/opt/splash.png
 if [ -f "$SPLASHIMAGE" ]; then
     echo "$SPLASHIMAGE exists"
@@ -48,6 +51,7 @@ else
     sudo ln splash.png /opt/splash.png
 fi
 
+#Link to service file for the splash screen
 SPLASHSERVICE=/etc/systemd/system/splashscreen.service
 if [ -f "$SPLASHSERVICE" ]; then
     echo "$SPLASHSERVICE exists"
@@ -56,7 +60,20 @@ else
     sudo ln splashscreen.service /etc/systemd/system/splashscreen.service
 fi
 
+#Enable Splash Screen
 sudo systemctl enable splashscreen
+
+#Move Concole output to tty3
+sudo sed -i 's/console=tty1/console=tty3/g' /boot/cmdline.txt
+
+#sed 's/$/ | COUNTRY/' file
+
+if grep -iq "loglevel=" /boot/cmdline.txt; then 
+    echo "Log level already set in /boot/cmdline.txt"
+else
+    sudo sed 's/$/ loglevel=3' file
+    #echo "do something else"
+fi
 
 echo ""
 echo "Installed Please Reboot"
